c516d3e57dc4dfb6336f2b3cad58bd067b039dd1
Stanley Lin <slin55055@gmail.com>
2017-12-13 14:31:27 -0500

    finished

--- find.js
+++ find.js
@@ -5,8 +5,18 @@ var boxWidth = box.offsetWidth;
 
 //hardcode target as center
 //randomize later
-var targetX = boxWidth / 2;
-var targetY = boxHeight / 2;
+var targetX = Math.random() * boxWidth;
+var targetY = Math.random() * boxHeight;
+
+var targetDiv = document.createElement("div");
+targetDiv.style.width = "50px";
+targetDiv.style.height = "50px";
+targetDiv.style.position = "absolute"
+targetDiv.style.left = targetX+"px";
+targetDiv.style.top = targetY+"px";
+targetDiv.style.cursor = "pointer";
+
+box.appendChild(targetDiv);
 
 var recalc = function(e) {
 	console.log("recalc");
@@ -27,11 +37,15 @@ var distance = function (x0, y0, x1, y1) {
 var maxDist = distance(boxHeight, boxWidth, 0, 0);
 
 var findIt = function(e) {
-	var scale = 255 - 
-		(255 * 
-		(distance(targetX, targetY, e.clientX, e.clientY)/maxDist));
-	
-	box.style.backgroundColor = "rgb(" + scale + ", " + scale + ", " + scale+ ")";
+	var scale = 100 -
+		(distance(targetX, targetY, e.clientX, e.clientY)/maxDist)*100;
+	console.log(scale);	
+	box.style.backgroundColor = "hsl(" + 130 + ", " + "43%" + ", " + scale+ "%)";
+	//alert("You found the cow!");
+};
+
+var found = function(e) {
+	alert("Moo!");
 };
 
 /*
@@ -40,5 +54,6 @@ your OTHER FXNS
 */
 
 box.addEventListener("mousemove", findIt);
+targetDiv.addEventListener("click", found);
 window.addEventListener("resize", recalc);
 
